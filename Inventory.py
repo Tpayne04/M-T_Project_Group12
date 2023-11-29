b025ba021219e78bd14a8d205091f9998b7a9d3c
Jason Barrow <135932616+DynamiteBog@users.noreply.github.com>
2023-11-29 13:34:14 -0600

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -1,16 +1,17 @@
 import sqlite3
+import sys
 
 class Inventory:
     def __init__(self):
         pass
 
-    def Inventory(self, databaseName='GroupProjectDatabase.db', tableName='Inventory'):
+    def Inventory(self, databaseName='Inventory.db', tableName='Inventory'):
         self.databaseName = databaseName
         self.tableName = tableName
-        self.conn = sqlite3.connect(r"C:\Program Files\DB Browser for SQLite\Methods and Tools of Software Development 2023 folder\GroupProjectDatabase.db")
+        self.conn = sqlite3.connect(r"C:\Program Files\DB Browser for SQLite\Methods and Tools of Software Development 2023 folder\Inventory.db")
         self.cursor = self.conn.cursor()
     
-        # Check if table already exists
+        # Check if table already exists, it should, this code is almost redundant now. Leaving it in case inventory's database fails.
         self.cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.tableName}';")
         if self.cursor.fetchone() is None:
             # If table doesn't exist, create it and insert a row
